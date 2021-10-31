49d4c44917785da0dfdd484bfa41b8cc9c4bf035
Jiyeon Moon <jiyeonmoon814@gmail.com>
2021-10-31 17:28:40 +0900

    Time: 80 ms (84.76%), Space: 41.5 MB (64.68%) - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -2,12 +2,6 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    for(i=0;i<nums.length;i++){
-        const count = nums.filter(num => num === nums[i])
-        
-        if(count.length === 1){
-            return nums[i]
-        }
-    }
-};
+const singleNumber = (nums) => {
+    return nums.reduce((prev,curr) => prev^curr)
+}
