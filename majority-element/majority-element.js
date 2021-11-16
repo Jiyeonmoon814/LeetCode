0af0c4e3fe77c76ebe7a5a1cfbc04849fad61c36
Jiyeon Moon <jiyeonmoon814@gmail.com>
2021-11-16 12:42:53 +0900

    Time: 64 ms(99.40%), Space: 42.4 MB(46.73%) - LeetHub

--- majority-element/majority-element.js
+++ majority-element/majority-element.js
@@ -16,5 +16,13 @@ var majorityElement = function(nums) {
         }
         obj[nums[i]] += 1
     }
+ 
+    // The Object.keys() method returns an array of a give object's own enumerable property names, 
+    // literated in the same order that a normal loop would 
     return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
+   // reduce(prevValue, currValue => smth with prev and curr ) 
+   // The reduce() method executes a user-supplied "reducer" callback function on each elemenbt of the array. 
+   // Passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array in a single value. 
+   // The reducer walks through the array element-by-element, at each step adding the current array value to the result from the previous step 
+   // (this result is the running sum of all the previous steps) until there are no more elements to add 
 };
